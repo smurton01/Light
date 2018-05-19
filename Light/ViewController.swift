d92d5178bd27ab3b0518c22073716ebbf88a6f68
Saya M <saya.murton@gmail.com>
2018-05-19 15:58:17 -0600

    `updateUI()`
    -made it so the button when launching says "Off"

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -29,6 +29,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
