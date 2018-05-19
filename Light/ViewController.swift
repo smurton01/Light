e7efb8b3db7a82ce9682e51c71e92c54359b41af
Saya M <saya.murton@gmail.com>
2018-05-19 15:30:47 -0600

    Action created
    -action called `button Pressed` created for button

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonPressed(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
